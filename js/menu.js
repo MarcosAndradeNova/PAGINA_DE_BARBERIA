3e78be8334586aa9ca17792149632abdc77e3ce4
MarcosAndradeNova <AndradeNovaMarcosDavid@gmail.com>
2025-12-24 10:00:18 -0400

    arreglando

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
