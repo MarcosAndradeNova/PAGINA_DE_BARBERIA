34e9e443a23ee6ca4e77b39dc4de3687c71963aa
MarcosAndradeNova <AndradeNovaMarcosDavid@gmail.com>
2026-01-04 19:11:40 -0400

    ajustes2

--- js/cortes.js
+++ js/cortes.js
@@ -11,3 +11,13 @@ checks.forEach(check => {
         }
     });
 });
+document.addEventListener('DOMContentLoaded', () => {
+const toggle = document.getElementById('menuToggle');
+const nav = document.querySelector('.navbar nav');
+
+if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+}
+});
