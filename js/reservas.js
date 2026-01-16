84e4c4085f9cbe6c7061517351bd6090b3a4bf87
MarcosAndradeNova <AndradeNovaMarcosDavid@gmail.com>
2026-01-16 18:19:39 -0400

    detalles

--- js/reservas.js
+++ js/reservas.js
@@ -13,7 +13,7 @@ calendario.setAttribute('min', hoySistema);
 const horarios = [
   "09:00","10:00","11:00","12:00",
   "13:00","14:00","15:00","16:00",
-  "17:00","18:00"
+  "17:00","18:00","19:00","20:00"
 ];
 
 // ====== CARGAR RESERVAS YA PAGADAS ======
